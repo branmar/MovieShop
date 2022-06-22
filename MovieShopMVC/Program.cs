35162b3984288f32c0bc38d66ba3c173e19a9f7a
User <User@LAPTOP>
2022-06-22 12:21:20 -0400

    Add Genre

--- MovieShopMVC/Program.cs
+++ MovieShopMVC/Program.cs
@@ -1,5 +1,6 @@
 using ApplicationCore.Contracts.Repository;
 using ApplicationCore.Contracts.Services;
+using ApplicationCore.Entities;
 using Infrastructure.Data;
 using Infrastructure.Repository;
 using Infrastructure.Services;
@@ -14,6 +15,8 @@ builder.Services.AddScoped<IMovieService, MovieService>();
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 builder.Services.AddScoped<ICastService, CastService>();
 builder.Services.AddScoped<ICastRepository, CastRepository>();
+builder.Services.AddScoped<IGenreService, GenreService>();
+builder.Services.AddScoped<IRepository<Genre>, Repository<Genre>>();
 
 builder.Services.AddDbContext<MovieShopDbContext>(options =>
 {
---
+++ MovieShopMVC/Views/Shared/Components/Genre/GenreViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Contracts.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Views.Shared.Components.Genre
+{
+    public class GenreViewComponent : ViewComponent
+    {
+        private readonly IGenreService _genreService;
+
+        public GenreViewComponent(IGenreService genreService)
+        {
+            _genreService = genreService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var genres = await _genreService.GetAllGenres();
+            return View(genres);
+        }
+    }
+}
